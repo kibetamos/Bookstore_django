cbc3fc5dcf47a1a605104e3ced73252deecedee0
kibetamos <amos.rotich@strathmore.edu>
2023-01-05 15:44:54 +0300

    Update: Added admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+CustomUser = get_user_model()
+
+class CustomUserAdmin(UserAdmin):
+    # The forms to add and change user instances
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email','username']
+
+    # The fields to be used in displaying the User model.
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
+
