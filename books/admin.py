8e89947866d6f57d4c508c156bd8c38931308a4a
kibetamos <amos.rotich@strathmore.edu>
2023-03-10 17:49:25 +0300

    Edited the books/admin page

--- books/admin.py
+++ books/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-
 # Register your models here.
 from .models import Book
-
-
-admin.site.register(Book)
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -14,9 +10,3 @@ class BookAdmin(admin.ModelAdmin):
 admin.site.register(Book, BookAdmin)
 
 
-# books/admin.py
-# from django.contrib import admin
-# from .models import Book
-# class BookAdmin(admin.ModelAdmin):
-# list_display = ("title", "author", "price",)
-# admin.site.register(Book, BookAdmin)
