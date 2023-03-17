8c3ba20270f6ad052e859ad5cd51830a62a14f91
kibetamos <amos.rotich@strathmore.edu>
2023-03-17 20:17:51 +0300

    update template

--- config/settings.py
+++ config/settings.py
@@ -11,12 +11,15 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 from environs import Env # new
 env = Env() # new
 env.read_env() # new
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 # Quick-start development settings - unsuitable for production
@@ -173,7 +176,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]  # new
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = BASE_DIR / "staticfiles"  # new
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"  # new
 
